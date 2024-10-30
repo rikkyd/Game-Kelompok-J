4366d5d871b244467a9f265ab01f1959b3d6b484
rikkyd <58492806+rikkyd@users.noreply.github.com>
2024-10-30 20:43:52 +0700

    a

--- Game Kita (J)/Assets/Programmer A/Script/PlayerController.cs
+++ Game Kita (J)/Assets/Programmer A/Script/PlayerController.cs
@@ -12,31 +12,91 @@ public class PlayerController : MonoBehaviour
     Vector2 movementInput;
     Rigidbody2D rb;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+    Animator animator;
+    bool isAttacking;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    private void FixedUpdate() // Fixedpdate changed to FixedUpdate
+    private void FixedUpdate()
     {
-        if (movementInput != Vector2.zero)
+        if (isAttacking)
         {
-            int count = rb.Cast(
-                movementInput,
-                movementFilter,
-                castCollisions,
-                moveSpeed * Time.fixedDeltaTime + collisionOffset
-            );
+            // During an attack, the player won't move
+            return;
+        }
 
-            rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+        bool isMoving = movementInput != Vector2.zero;
+
+        // Try to move in the direction of input if there is movement
+        if (isMoving)
+        {
+            bool success = TryMove(movementInput);
+
+            if (!success)
+            {
+                success = TryMove(new Vector2(movementInput.x, 0));
+
+                if (!success)
+                {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+            }
+
+            // Update animator direction parameters
+            animator.SetFloat("DirectionX", movementInput.x);
+            animator.SetFloat("DirectionY", movementInput.y);
+        }
+
+        // Update the isWalking parameter in the Animator
+        animator.SetBool("isWalking", isMoving);
+    }
+
+    private bool TryMove(Vector2 direction)
+    {
+        int count = rb.Cast(
+            direction,
+            movementFilter,
+            castCollisions,
+            moveSpeed * Time.fixedDeltaTime + collisionOffset
+        );
+
+        if (count == 0)
+        {
+            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 
-    void OnMove(InputValue movementValue) 
+    void OnMove(InputValue movementValue)
     {
         movementInput = movementValue.Get<Vector2>();
+    }
+
+    void OnAttack(InputValue attackValue)
+    {
+        if (attackValue.isPressed && !isAttacking)
+        {
+            StartCoroutine(Attack());
+        }
+    }
+
+    private IEnumerator Attack()
+    {
+        isAttacking = true;
+        animator.SetBool("isAttacking", true);
+
+        // Delay for the duration of the attack animation (adjust this duration)
+        yield return new WaitForSeconds(0.5f);
+
+        isAttacking = false;
+        animator.SetBool("isAttacking", false);
     }
 }
