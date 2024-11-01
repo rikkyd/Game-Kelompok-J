b369c90601996d3357b4c73c32753395ca477979
Rizza Dzalfa Septananda <rizzasyabia@gmail.com>
2024-11-01 20:25:44 +0700

    Penambahan RangedEnemy dan TrapArrow
    baru nambahin 2 dari 5 musuh yang direncanain

---
+++ Game Kita (J)/Assets/Programmer B/Script/Arrow Trap/ArrowScripts.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScripts : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    public float timetoLive = 4f;
+    public float timesinceSpawned = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += moveSpeed * transform.right * Time.deltaTime;
+        timesinceSpawned += Time.deltaTime;
+
+        if(timesinceSpawned >= timetoLive)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
