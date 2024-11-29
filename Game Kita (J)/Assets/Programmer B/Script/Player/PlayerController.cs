fedad0ae312e4a3b2551ab32f15775bf4767815a
JulianYang345 <126272283+JulianYang345@users.noreply.github.com>
2024-11-29 12:40:34 +0700

    - Player stats dan store sudah berfungsi penuh - bisa upgrade, reset, dan melihat info

--- Game Kita (J)/Assets/Programmer B/Script/Player/PlayerController.cs
+++ Game Kita (J)/Assets/Programmer B/Script/Player/PlayerController.cs
@@ -38,7 +38,6 @@ public class PlayerController : MonoBehaviour
         playerStats.health = playerStats.maxHealth; // Initialize the health
         //tambahan dari rizza
         originalSpeed = playerStats.moveSpeed;
-        UpdateUI(); // Update the display at the start
     }
 
     private void FixedUpdate()
@@ -72,6 +71,8 @@ public class PlayerController : MonoBehaviour
 
         animator.SetBool("isWalking", isMoving);
 
+        // Update the UI
+        playerUI.UpdateUI(playerStats.health, playerStats.maxHealth, playerStats.arrowCount, playerStats.coinCount, playerStats.potionCount);
     }
 
     private bool TryMove(Vector2 direction)
@@ -226,7 +227,6 @@ public class PlayerController : MonoBehaviour
         senjataPlayer.Shoot();
         playerStats.arrowCount--;
         Debug.Log("Arrow used. Remaining arrows: " + playerStats.arrowCount);
-        UpdateUI(); // Update the display after using an arrow
 
         yield return null; // Ensure the method yields a value
 
@@ -238,14 +238,18 @@ public class PlayerController : MonoBehaviour
     {
         playerStats.coinCount++;
         Debug.Log("Coin collected. Total coins: " + playerStats.coinCount);
-        UpdateUI(); // Update the display after collecting a coin
     }
 
     public void CollectPotion()
     {
-        playerStats.potionCount++;
-        Debug.Log("Potion collected. Total potions: " + playerStats.potionCount);
-        UpdateUI(); // Update the display after collecting a potion
+        if (playerStats.potionCount <= playerStats.maxPotionCount){
+            playerStats.potionCount++;
+            Debug.Log("Potion collected. Total potions: " + playerStats.potionCount);
+        }
+        else
+        {
+            Debug.Log("Potion count is at maximum.");
+        }
     }
 
     public void usePotion()
@@ -255,21 +259,10 @@ public class PlayerController : MonoBehaviour
             playerStats.potionCount--;
             playerStats.health++;
             Debug.Log("Potion used. Remaining potions: " + playerStats.potionCount);
-            UpdateUI(); // Update the display after using a potion
         }
         else
         {
             Debug.Log("No potions left to use.");
-        }
-    }
-
-    private void UpdateUI()
-    {
-        if (playerUI != null)
-        {
-            playerUI.UpdateArrowDisplay(playerStats.arrowCount);
-            playerUI.UpdateCoinDisplay(playerStats.coinCount);
-            playerUI.UpdatePotionDisplay(playerStats.potionCount);
         }
     }
 }
--- Game Kita (J)/Assets/Programmer B/Script/Player/PlayerUI.cs
+++ Game Kita (J)/Assets/Programmer B/Script/Player/PlayerUI.cs
@@ -1,13 +1,31 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerUI : MonoBehaviour
 {
     public TextMeshProUGUI arrowText; // Reference to the UI Text component for arrows
     public TextMeshProUGUI coinText;  // Reference to the UI Text component for coins
     public TextMeshProUGUI potionText; // Reference to the UI Text component for potions
-
+    public Slider HPBar; // Reference to the UI Slider component for health
     // Method to update the arrow display
+
+    public void UpdateUI(int hp, int maxHp, int arrowCount, int coinCount, int potionCount)
+    {
+        UpdateHPBar(hp, maxHp);
+        UpdateArrowDisplay(arrowCount);
+        UpdateCoinDisplay(coinCount);
+        UpdatePotionDisplay(potionCount);
+    }
+
+    public void UpdateHPBar(int currentHealth, int maxHealth)
+    {
+        if (HPBar != null)
+        {
+            HPBar.maxValue = maxHealth;
+            HPBar.value = currentHealth;
+        }
+    }
     public void UpdateArrowDisplay(int arrowCount)
     {
         if (arrowText != null)
